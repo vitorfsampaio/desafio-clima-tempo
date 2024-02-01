9d3f2afc416a07ef6d49397e2b842bedf8aa2beb
Vitor <vitorguilherme286@gmail.com>
2024-02-01 01:04:32 -0300

    last commit

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
-const pesquisaForm = document.querySelector('#pesquisaForm');
-const pesquisa = document.querySelector('#ipesquisa');
-const resultado = document.querySelector('#resultado')
+const searchForm = document.querySelector('#pesquisaForm');
+const search = document.querySelector('#ipesquisa');
+const result = document.querySelector('#resultado')
 
-async function BuscaCidade(pesquisa) {
+async function searchCity(search) {
     try {
-        const cidade = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${pesquisa.value}&limit=5&appid=12493105c51d36dc8befb9f811f0e8f7`)
+        const city = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${search.value}&limit=5&appid=12493105c51d36dc8befb9f811f0e8f7`)
 
-        const cidadeConvertida = await cidade.json()
+        const convertedCity = await city.json()
 
-        var lugar = cidadeConvertida[0].name;
+        var lat = convertedCity[0].lat;
+        var lon = convertedCity[0].lon;
 
-        var latitudeDoLugar = cidadeConvertida[0].lat;
-        var longitudeDoLugar = cidadeConvertida[0].lon;
+        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=12493105c51d36dc8befb9f811f0e8f7&lang=pt_br`)
 
-        const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitudeDoLugar}&lon=${longitudeDoLugar}&appid=12493105c51d36dc8befb9f811f0e8f7&lang=pt_br`)
+        const convertedApi = await api.json()
 
-        const apiConvertida = await api.json()
+        const currentDate = new Date();
 
-        const dataAtual = new Date();
+        const day = String(currentDate.getDate()).padStart(2, '0');
+        const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+        const year = String(currentDate.getFullYear()).slice(-4);
 
-        const dia = String(dataAtual.getDate()).padStart(2, '0');
-        const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-        const ano = String(dataAtual.getFullYear()).slice(-4);
-        
-        const dataFormatada = `${dia}/${mes}/${ano}`;
+        const fixedDate = `${day}/${month}/${year}`;
 
-        var nomeDaCidade = apiConvertida.name
-        var temperaturaDaCidade = (apiConvertida.main.temp - 273).toFixed(0)
-        var condicoesDaCidade = (apiConvertida.weather[0].description).toUpperCase()
-        
-        resultado.innerHTML = `
-            <p class="data">${dataFormatada}</p>
-            <h3 class="cidade">${nomeDaCidade}</h3>
-            <h1 class="temp">${temperaturaDaCidade}°</h1>
-            <p class="status">${condicoesDaCidade}</p>
+        var cityName = convertedApi.name
+        var cityTemp = (convertedApi.main.temp - 273).toFixed(0)
+        var cityConditions = (convertedApi.weather[0].description).toUpperCase()
+
+        result.innerHTML = `
+            <p class="data">${fixedDate}</p>
+            <h3 class="cidade">${cityName}</h3>
+            <h1 class="temp">${cityTemp}°</h1>
+            <p class="status">${cityConditions}</p>
         `
-        resultado.setAttribute('style', 'display: block');
+        result.setAttribute('style', 'display: block');
 
     } catch (error) {
         alert('Cidade não encontrada, tente novamente!')
-        pesquisa.value = ''
+        search.value = ''
     }
 }
 
-pesquisaForm.addEventListener('submit', (event) => {
+searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    BuscaCidade(pesquisa)
+    searchCity(search)
 });
